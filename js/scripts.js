2a4991252ed98c9838d2a2e2b710be1a38c035d2
meggilbert15 <59583008+meggilbert15@users.noreply.github.com>
2020-02-24 21:11:32 -0800

    Animation 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,40 @@
 console.log('OK');
+gsap.timeline()
+    .from('#bbLeaves', 
+          {  opacity: 0,
+            scale: 0,
+            transformOrigin:"bottom",
+            duration: .7, }
+         )
+         
+    .from('#bb',
+         { opacity: 0,
+            scale: 0,
+            transformOrigin:"bottom",
+            duration: 1,
+         }
+         )
+    .from('#bunchesLeaves', { 
+        opacity: 0,
+        scale: 0,
+        transformOrigin:"bottom",
+        duration: 1,
+     } )
+     .from('#bunches', 
+     { opacity: 0,
+        scale: 0,
+        transformOrigin:"bottom",
+        duration: 1,
+     }
+
+    );
+//     .from(".google", {
+//   opacity: 0,
+//   duration: 2,
+//   stagger: {
+//   each: .2,
+//   from: 'edges',
+//   }
+// });
+
+
